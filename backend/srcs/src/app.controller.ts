f7c9725686c1b0ac6b7dd0eb3e85c00bb79cefbe
dsaada <saada.dylan@gmail.com>
2023-02-11 11:14:45 +0100

    [FEATURE]WIP Started implementing link between back-end routes / prisma / postgres

--- backend/srcs/src/app.controller.ts
+++ backend/srcs/src/app.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './prisma/user.service';
+import { User as UserModel } from '@prisma/client'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get('/hello')
   getHello(): any {
     return this.appService.getHello();
   }
 
-  @Get('/user')
-  getUser(): any {
-    return this.appService.getUser()
+  @Get('/user/:id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Get('/user-list')
+  async getUserList(): Promise<UserModel[]> {
+    return this.userService.users({})
   }
 
   @Get()
--- backend/srcs/src/main.ts
+++ backend/srcs/src/main.ts
@@ -4,7 +4,6 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('api'); -> used to set a global prefix for the whole app
   await app.listen(3000);
 
   if (module.hot) {
