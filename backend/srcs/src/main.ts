887c350db60b75372c724680b260a0ea43f5bd35
dsaada <saada.dylan@gmail.com>
2023-02-14 08:17:10 +0100

    [FEATURE] Added global Prisma exception handling

--- backend/srcs/src/main.ts
+++ backend/srcs/src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 declare const module: any;
 
@@ -9,19 +10,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  //===== Validation Pipe to check for input errors =====
   app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
 
+  //===== Swagger Documentation of our API =====
   const config = new DocumentBuilder()
     .setTitle('PongChampions')
     .setDescription('The PongChampions API description')
     .setVersion('0.1')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  //===== Exception handling for prisma error code return =====
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3000);
 
+  //===== Hot Reload Module =====
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
---
+++ backend/srcs/src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,40 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const message = exception.message.replace(/\n/g, '');
+
+    switch (exception.code) {
+      //===== UNIQUE CONSTRAINTS VIOLATION =====
+      case 'P2002': {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
+        break;
+      }
+      //===== NOT FOUND INSIDE DATABASE =====
+      case 'P2025': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        })
+      }
+      default: {
+        console.log(exception.code);
+        // default 500 error code
+        super.catch(exception, host);
+        break;
+      }
+    }
+  }
+}
