c6b6e1da8914b78e120441ceb3fd471ecbe1299f
dsaada <saada.dylan@gmail.com>
2023-02-15 16:51:00 +0100

    [UPDATE] added /auth/me route protected by JwtAuthGuard

--- backend/srcs/src/auth/auth.controller.ts
+++ backend/srcs/src/auth/auth.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
-import { Auth42Guard, GoogleAuthGuard, LocalAuthGuard } from "./utils/guards";
+import { Auth42Guard, GoogleAuthGuard, JwtAuthGuard, LocalAuthGuard } from "./utils/guards";
 import { AuthService } from "./auth.service";
 import { Body } from "@nestjs/common";
-import { User } from ".prisma/client";
 import { ApiTags } from "@nestjs/swagger";
-import { LocalLogDto, LocalRegisterDto } from "./dto/log-user.dto";
+import { LocalRegisterDto } from "./dto/log-user.dto";
 import { ReturnUserEntity } from "src/users/entities/return-user.entity";
 
 @Controller('auth')
 @ApiTags('login')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
+
+    @Get('me')
+    @UseGuards(JwtAuthGuard)
+    acceptLoggedUser() {
+        return { msg: 'Welcome to the logged side of the site'}
+    }
 
     @Get('google/login')
     @UseGuards(GoogleAuthGuard)
