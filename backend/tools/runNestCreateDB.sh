7c7cb043f1329e6dcae690987115fedc507f1226
ddordain <ddordain@e1r11p1.clusters.42paris.fr>
2023-02-15 12:22:34 +0100

    [FIX] script for nestjs installation

--- backend/tools/runNestCreateDB.sh
+++ backend/tools/runNestCreateDB.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-npm install &
+npm install
+npm i -g @nestjs/cli --save-dev &&
+export PATH="$PATH:$(npm bin -g)"
 npx prisma db push && npx prisma generate &
 npx prisma db seed &
 npm run start:dev
