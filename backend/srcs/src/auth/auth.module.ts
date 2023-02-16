c14038567709f8a1cd78716d50b1a793ecd64813
dsaada <saada.dylan@gmail.com>
2023-02-16 14:38:30 +0100

    [UPDATE]WIP removed useless guards and routes in auth controller

--- backend/srcs/src/auth/auth.module.ts
+++ backend/srcs/src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { GoogleStrategy } from './utils/google-strategy';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './utils/local-strategy';
@@ -22,7 +21,6 @@ import { JwtStrategy } from './utils/jwt-strategy';
     providers: [
         PrismaService,
         AuthService,
-        GoogleStrategy,
         LocalStrategy,
         JwtStrategy,
     ],
