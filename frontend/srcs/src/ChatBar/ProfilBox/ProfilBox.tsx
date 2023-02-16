41131fdc7c10210ca3ca3729edfda2add0ecaed1
hugorclt <hugorecolet@gmail.com>
2023-02-16 13:11:11 +0100

    [UPDATE]added style on chatbar

--- frontend/srcs/src/ChatBar/ProfilBox/ProfilBox.tsx
+++ frontend/srcs/src/ChatBar/ProfilBox/ProfilBox.tsx
@@ -8,18 +8,22 @@ function ProfilBox() {
     <a href="#!">
       <div className="flex items-center">
         <div className="shrink-0">
-          <div className="w-10">
+          <div className="w-14">
             <CircularProgressbarWithChildren value={70} text="">
-              <img className="rounded-full w-8" src={logo}></img>
+              <img className="rounded-full w-12" src={logo}></img>
+              <div className="absolute outline outline-2 outline-orange-100 rounded-full bottom-0 right-0 w-3 h-3 bg-lime-500"></div>
+              <div className="absolute outline outline-2 outline-cyan-600 bg-white rounded-full top-0 right-0 w-4 h-4">
+                <div className="flex items-center justify-center w-4 h-4">
+                  <p className="text-gray-900 text-bold text-xs">1</p>
+                </div>
+              </div>
             </CircularProgressbarWithChildren>
           </div>
         </div>
-        <div className="flex flex-col ml-3">
-          <div className="grow ">
-            <p className="text-lg font-semibold text-orange-100 mb-1">
+        <div className="mx-6">
+            <p className="text-lg font-semibold text-orange-100 mb-0">
               hrecolet
             </p>
-          </div>
           <select
             className="outline-none bg-gray-900 text-orange-100 opacity-60 text-sm font-extralight"
             name="status"
