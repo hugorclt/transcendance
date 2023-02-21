8a4a597239d0c86e199ec263f814584f64c214fa
hugorclt <hugorecolet@gmail.com>
2023-02-21 09:59:57 +0100

    [UPDATE]added auth42 component

--- frontend/srcs/src/Login/Auth42/Auth42.tsx
+++ frontend/srcs/src/Login/Auth42/Auth42.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import Login42 from "react-42-login";
 
 function Auth42() {
+  const id42: string = process.env["REACT_APP_42UID"]!;
+
   return (
     <div>
+      <Login42
+        clientId={id42}
+        onFailure={() => {
+          console.log("NOO")
+        }}
+        onSuccess={() => {
+          console.log("Yes")
+
+        }}
+        route="/auth/42/login"
+        redirectUri="http://localhost:3002/"
+      >
+        42
+      </Login42>
     </div>
   );
 }
