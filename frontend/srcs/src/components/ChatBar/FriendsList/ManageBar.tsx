7171ec253c53eb1bce7454094db748eb11f5790a
Hugo Récolet <hugorecolet@gmail.com>
2023-02-24 20:46:11 +0100

    [UPDATE]friend list

--- frontend/srcs/src/components/ChatBar/FriendsList/ManageBar.tsx
+++ frontend/srcs/src/components/ChatBar/FriendsList/ManageBar.tsx
@@ -1,25 +1,64 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import {AiOutlineUsergroupAdd} from 'react-icons/ai'
 import {BiSearchAlt2} from 'react-icons/bi'
 import { IconContext } from 'react-icons/lib'
+import axios, { axiosPrivate } from '../../../services/axios';
+
+                // <button onClick={handleSearch}>
+                //     <IconContext.Provider value={{ color: "#E8C47C" }}>
+                //         <BiSearchAlt2 />
+                //     </IconContext.Provider>
+                // </button>
 
 function ManageBar() {
+    const [widthInvite, setWidthInvite] = useState("0%");
+    const [display, setDisplay] = useState("block");
+    const [username, setUsername] = useState("");
+    const focusRef = useRef<HTMLInputElement>(null);
+
+    const handleInvite = () => {
+        setWidthInvite('100%');
+        setDisplay("none");
+        if (!focusRef.current)
+            return ;
+        focusRef.current.focus();
+    }
+
+    const handleFocusOut = () => {
+        setWidthInvite("0%");
+        setDisplay("block");
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        axiosPrivate.post("/users/befriend", { username: username })
+    }
+
   return (
-    <div className='flex justify-between items-center mx-3'>
-        <div>
-            <h2 className='text-orange-100'>Friends</h2>
-        </div>
-        <div className='mx-3'>
-            <button className='mx-3'>
-                <IconContext.Provider value={{ color: "#E8C47C" }}>
-                    <AiOutlineUsergroupAdd />
-                </IconContext.Provider>
-            </button>
-            <button>
-                <IconContext.Provider value={{ color: "#E8C47C" }}>
-                    <BiSearchAlt2 />
-                </IconContext.Provider>
-            </button>
+    <div>
+        <div className='flex justify-between items-center'>
+            <div>
+                <h2 className='text-orange-100 mx-3 mt-1 transition-all' style={{display:display}}>Friends</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input  
+                        onChange={(e) => {setUsername(e.target.value)}}
+                        type="text"
+                        name="username"
+                         ref={focusRef} onBlur={handleFocusOut} className="mt-0" style={{
+                        width: widthInvite,
+                        transition: "1s",
+                        backgroundColor: "#2d384c",
+                    }}
+                />
+            </form>
+            <div className='flex justify-end mt-1'>
+                <button style={{display:display}} className='mx-3 transition-all' onClick={handleInvite}>
+                    <IconContext.Provider value={{ color: "#E8C47C" }}>
+                        <AiOutlineUsergroupAdd />
+                    </IconContext.Provider>
+                </button>
+            </div>
         </div>
     </div>
   )
