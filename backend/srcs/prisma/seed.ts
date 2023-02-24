9317621fe48ec6b65733d14d6ab2c380b03644a3
ddordain <ddordain@e1r9p3.clusters.42paris.fr>
2023-02-24 17:53:50 +0100

    [WORKING ON] friend

--- backend/srcs/prisma/seed.ts
+++ backend/srcs/prisma/seed.ts
@@ -1,5 +1,5 @@
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
