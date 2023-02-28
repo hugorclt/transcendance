f3d926124cdf43c6ac0385e56a670b548b0b7ddd
dsaada <saada.dylan@gmail.com>
2023-02-28 18:52:42 +0100

    [WIP] Starting to implement lobby (prisma + ressource)

--- backend/srcs/src/lobby/entities/lobby.entity.ts
+++ backend/srcs/src/lobby/entities/lobby.entity.ts
@@ -1,14 +1,23 @@
 import { Lobby } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
-import { Mode } from '@prisma/client';
+import { Mode, MapName } from '@prisma/client';
 
 export class LobbyEntity implements Lobby {
   @ApiProperty()
   id: string;
 
-  @ApiProperty({ default: Mode.CLASSIC })
-  mode: Mode;
-
   @ApiProperty()
   ownerId: string;
+
+  @ApiProperty({ default: 2 })
+  nbPlayers: number = 2;
+
+  @ApiProperty({ default: 180 })
+  maxDuration: number = 180;
+
+  @ApiProperty({ default: Mode.CLASSIC })
+  mode: Mode = Mode.CLASSIC;
+
+  @ApiProperty({ default: MapName.CLASSIC })
+  map: MapName = MapName.CLASSIC;
 }
