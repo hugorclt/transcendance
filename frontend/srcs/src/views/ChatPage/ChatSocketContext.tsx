4473f4d3f2ed679ca1bb257cd56356cf65dc41b0
dsaada <saada.dylan@gmail.com>
2023-03-02 09:33:19 +0100

    [CLEANER] cleaned useless files

--- frontend/srcs/src/views/ChatPage/ChatSocketContext.tsx
+++ frontend/srcs/src/views/ChatPage/ChatSocketContext.tsx
@@ -1,16 +1,16 @@
-import { AxiosError, AxiosResponse } from 'axios';
-import React, { createContext } from 'react';
-import { io, Socket } from 'socket.io-client';
-import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { AxiosResponse } from "axios";
+import React, { createContext } from "react";
+import { io, Socket } from "socket.io-client";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 async function initializeSocket(axiosPrivate: any) {
   try {
     const res: AxiosResponse = await axiosPrivate.get("auth/me");
     const userId = res.data.id;
-    const socket = io('http://localhost:3000/status', {
+    const socket = io("http://localhost:3000/status", {
       query: { userId },
     });
-    return (socket);
+    return socket;
   } catch (err: any) {
     console.log("Error fetching user ID: ", err);
   }
@@ -18,13 +18,17 @@ async function initializeSocket(axiosPrivate: any) {
 
 export const ChatSocketContext = createContext<Socket | null>(null);
 
-export function ChatSocketProvider({ children }: { children: React.ReactNode }) {
+export function ChatSocketProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [socket, setSocket] = React.useState<Socket | null>(null);
   const axiosPrivate = useAxiosPrivate();
 
   React.useEffect(() => {
     async function initSocket() {
-      const s : any = await initializeSocket(axiosPrivate);
+      const s: any = await initializeSocket(axiosPrivate);
       setSocket(s);
     }
     initSocket();
