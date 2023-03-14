4149e30ffd920c27256de186f81640c17b592692
dsaada <saada.dylan@gmail.com>
2023-03-14 15:23:30 +0100

    .

--- frontend/srcs/src/views/MainPage/MainPage.tsx
+++ frontend/srcs/src/views/MainPage/MainPage.tsx
@@ -1,14 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import MainLayout from "../../layouts/MainLayout/MainLayout";
-import { MainProvider } from "./MainContext";
-
-function MainPage() {
-  return (
-    <MainProvider>
-      <MainLayout />
-    </MainProvider>
-=======
 import { Outlet } from "react-router";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import { StateProvider } from "./StateProvider";
@@ -20,7 +10,6 @@ function MainPage() {
         <Outlet />
       </StateProvider>
     </MainLayout>
->>>>>>> hugo
   );
 }
 
