177bcd396886f86f1727ba76033e07de6685854f
dsaada <saada.dylan@gmail.com>
2023-03-18 16:40:38 +0100

    [UPDATE]WIP Lobby invitations now get sent to users and can be accepted to join lobby (new route)

--- frontend/srcs/src/components/ChatBar/FriendsList/FriendNotifications/FriendsNotifications.tsx
+++ frontend/srcs/src/components/ChatBar/FriendsList/FriendNotifications/FriendsNotifications.tsx
@@ -2,28 +2,64 @@ import React, { useContext, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SocketContext } from "../../../../services/Auth/SocketContext";
+import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
+import { AxiosError, AxiosResponse } from "axios";
 
 function FriendNotifications() {
   const socket = useContext(SocketContext);
-
-  const acceptRequest = (username: string) => {
+  const axiosPrivate = useAxiosPrivate();
+  const acceptFriendRequest = (username: string) => {
     socket?.emit("friend-request-reply", {
       fromUsername: username,
       isReplyTrue: true,
     });
   };
 
-  const notify = (username: string) =>
+  const acceptLobbyRequest = (lobbyId: string, userId: string) => {
+    axiosPrivate
+      .post("/lobbies/join", { lobbyId: lobbyId, userId: userId })
+      .then((response: AxiosResponse) => {
+        console.log("success joining lobby: ", JSON.stringify(response.data));
+      })
+      .catch((error: AxiosError) => {
+        console.log("error joining lobby: ", JSON.stringify(error.cause));
+      });
+  };
+
+  //========== FRIEND NOTIFICATION  ==============
+  const notifyFriend = (invitation: any) =>
     toast(
       <>
         <p>
-          <strong>{username}</strong> has sent you a friend request
+          <strong>{invitation.userFromUsername}</strong> has sent you a friend
+          request
         </p>
         <div className="flex justify-around">
           <button
             className="text-green-800"
             onClick={() => {
-              acceptRequest(username);
+              acceptFriendRequest(invitation.userFromUsername);
+            }}
+          >
+            Accept
+          </button>
+          <button className="text-red-800">Refuse</button>
+        </div>
+      </>
+    );
+
+  //=============== LOBBY NOTIFICATION ================
+  const notifyLobby = (invitation: any) =>
+    toast(
+      <>
+        <p>
+          <strong>{invitation.userFromUsername}</strong> invited you to a lobby
+        </p>
+        <div className="flex justify-around">
+          <button
+            className="text-green-800"
+            onClick={() => {
+              acceptLobbyRequest(invitation.lobbyId, invitation.userId);
             }}
           >
             Accept
@@ -36,7 +72,11 @@ function FriendNotifications() {
   useEffect(() => {
     socket?.on("invitation", (invitation) => {
       console.log("received invitation: ", invitation);
-      notify(invitation.userFromUsername);
+      if (invitation.type == "FRIEND") {
+        notifyFriend(invitation);
+      } else if (invitation.type == "LOBBY") {
+        notifyLobby(invitation);
+      }
     });
 
     return () => {
