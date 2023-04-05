acb67766aa93909acbdc9bcb7adc411122187f0d
dsaada <saada.dylan@gmail.com>
2023-04-05 17:03:34 +0200

    [SAVE]

--- frontend/srcs/src/layouts/MainLayout/MainLayout.style.ts
+++ frontend/srcs/src/layouts/MainLayout/MainLayout.style.ts
@@ -24,7 +24,7 @@ export const PageContainer = styled.div`
   background-color: ${COLORS.darkergrey};
   flex-grow: 1;
   padding: 16px;
-  min-width: 300px;
+  min-width: 280px;
   @media (max-width: ${screenSize.laptop}) {
     width: auto;
   }
