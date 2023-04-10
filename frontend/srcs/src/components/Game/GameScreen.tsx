26ea21c75f79fb4be940cdc988ce226aec24c8d8
hugorclt <hugorecolet@gmail.com>
2023-04-10 07:50:36 +0200

    [UPDATE]added possibility to sort rank by descending order

--- frontend/srcs/src/components/Game/GameScreen.tsx
+++ frontend/srcs/src/components/Game/GameScreen.tsx
@@ -10,14 +10,47 @@ import { Vector3 } from "three";
 import Scene from "./Scene";
 
 const GameScreen = () => {
+  const screenRef = useRef<any>();
+  const downloadRef = useRef<any>();
+
+  useEffect(() => {
+    if (screenRef.current == undefined || screenRef.current == null) return;
+    const stream = screenRef.current!.captureStream(60);
+    const recordedChunks = [];
+    const options = {
+      mimeType: "video/webm",
+    };
+    const mediaRecorder = new MediaRecorder(stream, options);
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+
+    function handleDataAvailable(event: any) {
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+      }
+    }
+
+    setTimeout((event) => {
+      mediaRecorder.stop();
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      var url = URL.createObjectURL(blob);
+      downloadRef.current.href = url;
+      downloadRef.current.download("paddle.webm");
+    }, 9000);
+  }, [screenRef]);
+
   return (
-    <Canvas
-      tabIndex={0}
-      camera={{ position: [0, 6.5, 53] }}
-      style={{ background: COLORS.background, height: "100vh" }}>
-      {/* <Game /> */}
-      {/* <SeaMap /> */}
-      {/* <Stars
+    <>
+      <Canvas
+        ref={screenRef}
+        tabIndex={0}
+        camera={{ position: [0, 6.5, 53] }}
+        style={{ background: COLORS.background, height: "100vh" }}>
+          {/* <Game /> */}
+          {/* <SeaMap /> */}
+          {/* <Stars
         radius={100}
         depth={50}
         count={5000}
@@ -26,8 +59,10 @@ const GameScreen = () => {
         fade
         speed={1}
       /> */}
-      <Scene />
-    </Canvas>
+          <Scene />
+      </Canvas>
+      <a ref={downloadRef}>Download</a>
+    </>
   );
 };
 
--- frontend/srcs/src/components/Game/Scene.tsx
+++ frontend/srcs/src/components/Game/Scene.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useRef } from "react";
-import { OrbitControls, PerspectiveCamera, SoftShadows } from "@react-three/drei";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  SoftShadows,
+} from "@react-three/drei";
 import PaddleScene from "./Components/sceneComponents/Paddle";
 import { Camera, Vector3 } from "three";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 function Scene() {
-    const ref = useRef<any>();
+  const ref = useRef<any>();
 
-    useEffect(() => {
-        ref.current!.lookAt(new Vector3(0, 0, 0))
-    }, [])
+  useEffect(() => {
+    ref.current!.lookAt(new Vector3(0, 0, 0));
+  }, []);
   return (
     <>
       <EffectComposer multisampling={0}>
@@ -17,7 +21,12 @@ function Scene() {
       </EffectComposer>
       <hemisphereLight args={["#ffff", 0.6]} />
       {/* <OrbitControls /> */}
-      <PerspectiveCamera ref={ref} makeDefault fov={100} position={[0, 5, 15]} />
+      <PerspectiveCamera
+        ref={ref}
+        makeDefault
+        fov={100}
+        position={[0, 5, 15]}
+      />
       <PaddleScene width={7} length={3} startPos={new Vector3(0, 0, 0)} />
       <SoftShadows />
     </>
