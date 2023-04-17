993773001acf8cd5b5c7c897e897a959ef477a89
hugorclt <hugorecolet@gmail.com>
2023-04-17 12:02:31 +0200

    [nul]

--- frontend/srcs/src/components/Game/Components/CollisionDisk/CollisionDisk.tsx
+++ frontend/srcs/src/components/Game/Components/CollisionDisk/CollisionDisk.tsx
@@ -19,47 +19,35 @@ function CollisionDisk({ gameInfo }: TPropsCollisionDisk) {
       meshRef.current.position.y = data.position._y;
       meshRef.current.position.z = data.position._z;
 
+      if (data.direction._z)
+        return ;
       if (data.direction._x) {
         if (data.direction._x > 0) {
-          // rotate - 90 y
-          meshRef.current.rotation.x = degToRad(90);
+          meshRef.current.rotation.y = degToRad(90);
         } else {
-          //rotation 90 y
-          meshRef.current.rotation.x = degToRad(-90);
+          meshRef.current.rotation.y = degToRad(-90);
         }
       } else if (data.direction._y) {
         if (data.direction._y > 0) {
           // rotate - 90 x
-          meshRef.current.rotation.y = degToRad(90);
+          meshRef.current.rotation.x = degToRad(-90);
         } else {
-          meshRef.current.rotation.y = degToRad(-90);
+          meshRef.current.rotation.x = degToRad(90);
 
           //rotation 90 x
         }
-      } else if (data.direction._z) {
-        if (data.direction._z > 0) {
-          meshRef.current.rotation.z = degToRad(90);
-          // rotate - 90 y
-        } else {
-          meshRef.current.rotation.z = degToRad(-90);
-
-          //rotation 90 y
-        }
       }
-      meshRef.current.rotation.x = data.position._x;
-      meshRef.current.rotation.y = data.position._y;
-      meshRef.current.rotation.z = data.position._z;
-      meshRef.current.scale.x += 0.1;
-      meshRef.current.scale.y += 0.1;
-      meshRef.current.scale.z += 0.1;
+      meshRef.current.scale.x = 0.5;
+      meshRef.current.scale.y = 0.5;
+      meshRef.current.scale.z = 0.5;
       const id = setInterval(() => {
         console.log("colission animation loop");
 
-        meshRef.current.scale.x += 0.1;
-        meshRef.current.scale.y += 0.1;
-        meshRef.current.scale.z += 0.1;
+        meshRef.current.scale.x += 0.05;
+        meshRef.current.scale.y += 0.05;
+        meshRef.current.scale.z += 0.05;
         // meshRef.current.geometry.parameters.radius += 0.1;
-        matRef.current.opacity -= 0.1;
+        // matRef.current.opacity -= 0.1;
       }, 100);
 
       setTimeout(() => {
@@ -68,8 +56,11 @@ function CollisionDisk({ gameInfo }: TPropsCollisionDisk) {
         meshRef.current.scale.y = 0;
         meshRef.current.scale.z = 0;
         matRef.current.opacity = 0;
+        meshRef.current.rotation.x = 0;
+        meshRef.current.rotation.y = 0;
+        meshRef.current.rotation.z = 0;
         clearInterval(id);
-      }, 500);
+      }, 1000);
     });
     return () => {
       socket?.off("collision");
@@ -78,14 +69,14 @@ function CollisionDisk({ gameInfo }: TPropsCollisionDisk) {
 
   return (
     <mesh ref={meshRef}>
-      <circleGeometry args={[0.5]} />
+      <circleGeometry args={[0.8]} />
       <meshToonMaterial
-        color={"red"}
+        color={"#ff0101"}
         ref={matRef}
         opacity={1}
         emissiveIntensity={0}
         toneMapped={false}
-        transparent={true}
+        transparent={false}
       />
     </mesh>
   );
