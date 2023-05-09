e4380a274756126b51c598a353824125622b9af8
dsaada <saada.dylan@gmail.com>
2023-05-09 10:52:28 +0200

    [UPDATE] added selection security conditional click on invite

--- frontend/srcs/src/components/Lobby/TeamBuilder/TeamBuilderButtons/InviteFriendsButton/InviteFriendCard/InviteFriendCardStyle.ts
+++ frontend/srcs/src/components/Lobby/TeamBuilder/TeamBuilderButtons/InviteFriendsButton/InviteFriendCard/InviteFriendCardStyle.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { COLORS } from "../../../../../../colors";
+import { screenSize } from "../../../../../../mediaSize";
 
 export const InviteFriendCardContainer = styled.div`
-  width: 200px;
   height: 50px;
   display: flex;
   cursor: pointer;
@@ -10,6 +10,15 @@ export const InviteFriendCardContainer = styled.div`
   justify-content: start;
   border-radius: 8px;
   padding: 8px;
+  @media (max-width: ${screenSize.tablet}) {
+    width: 180px;
+  }
+  @media (${screenSize.tablet} < width < ${screenSize.laptop}) {
+    width: 330px;
+  }
+  @media (min-width: ${screenSize.laptop}) {
+    width: 460px;
+  }
 
   &:hover {
     background-color: ${COLORS.secondary};
