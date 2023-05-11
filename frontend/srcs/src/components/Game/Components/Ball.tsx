8059181a0ea0495ef6a9d9eb4c7e3bc62a13b1d5
hugorclt <hugorecolet@gmail.com>
2023-05-11 16:27:30 +0200

    [UPDATE]added trail

--- frontend/srcs/src/components/Game/Components/Ball.tsx
+++ frontend/srcs/src/components/Game/Components/Ball.tsx
@@ -5,6 +5,8 @@ import { LobbySocketContext } from "../../../services/Lobby/LobbySocketContext";
 import { Object3D } from "./Assets/interfaces";
 import { createMeshComponent } from "./Assets/meshGenerator";
 import { useState } from "react";
+import { Trail } from "@react-three/drei";
+import * as THREE from "three";
 
 type BallProps = {
   ball: Object3D;
@@ -13,6 +15,7 @@ type BallProps = {
 const Ball = (props: BallProps) => {
   const ballRef = useRef<Mesh>(null!);
   const socket = useContext(LobbySocketContext);
+  const [trail, setTrail] = useState(0xfffff);
   var velocity;
 
   useFrame(({ clock }) => {
@@ -33,6 +36,7 @@ const Ball = (props: BallProps) => {
         data.ball.velocity.y,
         data.ball.velocity.z
       );
+      setTrail(data.superPowerTouch);
     });
 
     return () => {
@@ -40,7 +44,23 @@ const Ball = (props: BallProps) => {
     };
   }, [socket]);
 
-  return <>{createMeshComponent(props.ball, ballRef, false)}</>;
+  return (
+    <>
+      <Trail
+        width={0.2} // Width of the line
+        color={new THREE.Color(trail)} // Color of the line
+        length={1} // Length of the line
+        decay={1} // How fast the line fades away
+        local={false} // Wether to use the target's world or local positions
+        stride={0} // Min distance between previous and current point
+        interval={1} // Number of frames to wait before next calculation
+        target={undefined} // Optional target. This object will produce the trail.
+        attenuation={(width) => width} // A function to define the width in each point along it.
+      >
+        {createMeshComponent(props.ball, ballRef, false)}
+      </Trail>
+    </>
+  );
 };
 
 export default Ball;
