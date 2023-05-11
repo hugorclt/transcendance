a52ddaba11f1198cef3bcf77c8167c3422a392b6
hugorclt <hugorecolet@gmail.com>
2023-05-11 15:48:15 +0200

    [UPDATE]pushed

--- frontend/srcs/src/components/Game/Scoreboard.tsx
+++ frontend/srcs/src/components/Game/Scoreboard.tsx
@@ -20,28 +20,30 @@ function Scoreboard() {
   const socket = useContext(LobbySocketContext);
   const [team1score, setTeam1score] = useState(0);
   const [team2score, setTeam2score] = useState(0);
+  const [charge, setCharge] = useState(0);
 
-useEffect(() => {
-  if (scoreChanged === true) {
-    const blinkTimeout = setTimeout(() => {
-      setScoreChanged(false);
-    }, 0);
-    return () => clearTimeout(blinkTimeout);
-  }
-});
+  useEffect(() => {
+    if (scoreChanged === true) {
+      const blinkTimeout = setTimeout(() => {
+        setScoreChanged(false);
+      }, 0);
+      return () => clearTimeout(blinkTimeout);
+    }
+  });
 
   useEffect(() => {
     socket?.on("frame", (frame) => {
       if (team1score != frame.score.team1 || team2score != frame.score.team2) {
         setTeam1score(frame.score.team1);
         setTeam2score(frame.score.team2);
+        setCharge(frame.charge);
         setScoreChanged(true);
       }
     });
 
     return () => {
       socket?.off("frame");
-    }
+    };
   }, [socket]);
 
   return (
@@ -58,10 +60,30 @@ useEffect(() => {
           left: "50%",
           transform: "translate(-50%, -50%)",
         }}>
-        Team 1&nbsp;&nbsp;&nbsp;
+        <div>
+          Team 1&nbsp;&nbsp;&nbsp;
+          <div
+            style={{
+              width: "10px",
+              height: "5px",
+              backgroundColor: charge >= 1 ? "blue" : "grey",
+            }}></div>
+          <div
+            style={{
+              width: "10px",
+              height: "5px",
+              backgroundColor: charge >= 2 ? "blue" : "grey",
+            }}></div>
+          <div
+            style={{
+              width: "10px",
+              height: "5px",
+              backgroundColor: charge >= 3 ? "blue" : "grey",
+            }}></div>
+        </div>
         <Score value={team1score} blink={scoreChanged} /> -{" "}
         <Score value={team2score} blink={scoreChanged} />
-        &nbsp;&nbsp;&nbsp;Team 2
+        <div>&nbsp;&nbsp;&nbsp;Team 2</div>
       </div>
       <Heptahedre />
     </>
--- frontend/srcs/src/components/Lobby/Selector/PaddleSelector/PaddleSelectorCards/PaddleSelectorCards.tsx
+++ frontend/srcs/src/components/Lobby/Selector/PaddleSelector/PaddleSelectorCards/PaddleSelectorCards.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { SelectorCardsContainer } from "./PaddleSelector.style";
-import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
 
 function PaddleSelectorCards({ name, img, isSelected }) {
   return (
@@ -11,6 +10,7 @@ function PaddleSelectorCards({ name, img, isSelected }) {
         backgroundSize: "cover",
         boxShadow: isSelected ? "0px 0px 8px 0px #DB504A" : "",
         transition: "all 250ms ease-in",
+        filter: name == "Base Paddle" ? "grayscale(100%)" : "",
       }}>
       </SelectorCardsContainer>
   );
