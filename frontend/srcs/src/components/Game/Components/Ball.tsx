06cf704d8131ba916609741deef27483546eaa9d
hugorclt <hugorecolet@gmail.com>
2023-05-12 10:48:01 +0200

    [UPDATE]lol

--- frontend/srcs/src/components/Game/Components/Ball.tsx
+++ frontend/srcs/src/components/Game/Components/Ball.tsx
@@ -67,23 +67,7 @@ const Ball = (props: BallProps) => {
     };
   }, [socket]);
 
-  return (
-    <>
-      <Trail
-        width={0.2} // Width of the line
-        color={new THREE.Color(trail)} // Color of the line
-        length={1} // Length of the line
-        decay={1} // How fast the line fades away
-        local={false} // Wether to use the target's world or local positions
-        stride={0} // Min distance between previous and current point
-        interval={1} // Number of frames to wait before next calculation
-        target={undefined} // Optional target. This object will produce the trail.
-        attenuation={(width) => width} // A function to define the width in each point along it.
-      >
-        {createMeshComponent(props.ball, ballRef, false)}
-      </Trail>
-    </>
-  );
+  return <>{createMeshComponent(props.ball, ballRef, false)}</>;
 };
 
 export default Ball;
