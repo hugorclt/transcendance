6a3b783a4a8d0c8a5bffd7d26d18d3b7c85fa294
hugorclt <hugorecolet@gmail.com>
2023-05-16 12:55:49 +0200

    [UPDATE]fix dedoublement socket

--- frontend/srcs/src/services/Auth/ProvideSockets.tsx
+++ frontend/srcs/src/services/Auth/ProvideSockets.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useLocation, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { SocketProvider } from "./SocketContext";
 import { LobbySocketProvider } from "../Lobby/LobbySocketContext";
 
 function ProvideSockets() {
-  const location = useLocation();
-
   return (
     <SocketProvider>
       <LobbySocketProvider>
