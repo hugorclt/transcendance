8526c2250be12e29f727b5875f876fa131f335af
hugorclt <hugorecolet@gmail.com>
2023-05-18 12:50:55 +0200

    [UPDATE]added level on profile

--- frontend/srcs/src/components/Profile/TopProfile/ProfileBox/ProfileBox.tsx
+++ frontend/srcs/src/components/Profile/TopProfile/ProfileBox/ProfileBox.tsx
@@ -12,12 +12,23 @@ export type TProfileBoxProps = {
 };
 
 export function ProfileBox({ user }: TProfileBoxProps) {
+  const calculateLevel = () => {
+    const xp = Math.max(
+      1,
+      Math.log(user.exp / 500) / Math.log(Math.pow(2, 1 / 5)) + 1
+    );
+    return xp;
+  };
   return (
     <ProfileBoxContainer>
-      <img style={{width: "175px", height: "175px", borderRadius: "100px"}} src={getImageBase64(user.avatar)}></img>
+      <img
+        style={{ width: "175px", height: "175px", borderRadius: "100px" }}
+        src={getImageBase64(user.avatar)}></img>
       <h3>{user.username}</h3>
-      <p>{user.status}</p>
-      <div>{user.exp}</div>
+      <div style={{display: "flex"}}>
+        <p style={{marginRight: "16px"}}>{user.status}</p>
+        <div>Level: {Math.floor(calculateLevel())}</div>
+      </div>
     </ProfileBoxContainer>
   );
 }
